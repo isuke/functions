b7506fa0983e8e83dde6b737dfec143f22ee5750
isuke <isuke770@gmail.com>
2018-02-17 16:06:08 +0900

    make dir when not exists

--- functions.zsh
+++ functions.zsh
@@ -1,6 +1,11 @@
 FUNCTIONS_ROOT_PATH=$HOME/functions
 BIN_PATH=$HOME/bin
 
+if [[ ! -d $BIN_PATH ]]; then
+  echo "Make $BIN_PATH"
+  mkdir $BIN_PATH
+fi
+
 echo "Copy src to $BIN_PATH"
 
 cp $FUNCTIONS_ROOT_PATH/src/* $BIN_PATH
